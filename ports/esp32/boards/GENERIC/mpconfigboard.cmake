7aa07ef6a41827df6514b4b8bc5702537ff68c3d
Christof Tinnes <schwinnez@yahoo.de>
2021-03-16 09:57:46 +0000

    Improvement: Deactivating Bluetooth for build, since it is not used. This improves free heap and binary size.

--- ports/esp32/boards/GENERIC/mpconfigboard.cmake
+++ ports/esp32/boards/GENERIC/mpconfigboard.cmake
@@ -1,6 +1,7 @@
 set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.base
-    boards/sdkconfig.ble
+    # boards/sdkconfig.ble
+    boards/GENERIC/sdkconfig.board
 )
 
 set(MICROPY_FROZEN_MANIFEST ${MICROPY_PORT_DIR}/boards/manifest.py)
