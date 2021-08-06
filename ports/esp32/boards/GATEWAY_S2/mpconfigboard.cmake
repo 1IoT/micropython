12676efbd1cda92b99d4c04ad2fae4cacfca7b0b
Christof Tinnes <schwinnez@yahoo.de>
2021-08-06 09:24:18 +0000

    Adding new board definitions.

---
+++ ports/esp32/boards/GATEWAY_S2/mpconfigboard.cmake
@@ -0,0 +1,12 @@
+set(IDF_TARGET esp32s2)
+
+set(SDKCONFIG_DEFAULTS
+    boards/sdkconfig.base
+    boards/sdkconfig.usb
+    boards/sdkconfig.spiram_sx
+    boards/GATEWAY_S2/sdkconfig.board
+)
+
+if(NOT MICROPY_FROZEN_MANIFEST)
+    set(MICROPY_FROZEN_MANIFEST ${MICROPY_PORT_DIR}/boards/manifest.py)
+endif()
